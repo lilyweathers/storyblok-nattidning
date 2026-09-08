08f4d5d3e75122e950371fbd021dc07d29c7a53f
Samuel <samuel_somy93@hotmail.com>
2026-09-08 10:55:53 +0200

    feat: enhance Page component to conditionally render content based on body length

--- src/components/Page.jsx
+++ src/components/Page.jsx
@@ -2,12 +2,21 @@ import {
 	storyblokEditable,
 	StoryblokServerComponent,
 } from '@storyblok/react/rsc';
+import Link from 'next/link';
 
 const Page = ({ blok }) => (
 	<main {...storyblokEditable(blok)}>
-		{blok.body?.map((nestedBlok) => (
-			<StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
-		))}
+		{blok.body?.length ? (
+			blok.body.map((nestedBlok) => (
+				<StoryblokServerComponent blok={nestedBlok} key={nestedBlok._uid} />
+			))
+		) : (
+			<section>
+				<h1>Nättidning</h1>
+				<p>En nyhetssida byggd med Next.js och Storyblok.</p>
+				<Link href="/articles">Gå till artiklar</Link>
+			</section>
+		)}
 	</main>
 );
 
