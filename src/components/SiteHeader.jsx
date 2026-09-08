9599082cbfebe0c14efb87d44c452ce7cf9a6d01
Samuel <samuel_somy93@hotmail.com>
2026-09-08 11:49:55 +0200

    feat: add author navigation menu

--- src/components/SiteHeader.jsx
+++ src/components/SiteHeader.jsx
@@ -9,12 +9,21 @@ export default function SiteHeader() {
 				<div>
 					<Link href="/">Start</Link>
 
-					<details>
+					<details name="main-navigation">
 						<summary>Artiklar</summary>
 						<div>
 							<Link href="/articles">Alla artiklar</Link>
 							<Link href="/categories/nyheter">Nyheter</Link>
 							<Link href="/categories/guide">Guide</Link>
+						</div>
+					</details>
+
+					<details name="main-navigation">
+						<summary>Authors</summary>
+						<div>
+							<Link href="/authors">Alla författare</Link>
+							<Link href="/authors/erik-lind">Erik Lind</Link>
+							<Link href="/authors/anna-berg">Anna Berg</Link>
 						</div>
 					</details>
 
